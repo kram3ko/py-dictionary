ac99cd65351600e89ccd5b966e6074f4cd966cf7
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-03-11 18:37:38 +0200

    update solution

--- app/main.py
+++ app/main.py
@@ -28,12 +28,14 @@ class Dictionary:
             {
                 element.key: element.value
                 for element in self.hash_table
-                if element
+                if isinstance(element, Node)
             }
         )
 
     def renew_capacity(self) -> None:
-        old_hash_table = [node for node in self.hash_table if node]
+        old_hash_table = [
+            node for node in self.hash_table if isinstance(node, Node)
+        ]
         self.capacity *= 2
         self.hash_table = [None] * self.capacity
         self.threshold = self.capacity * self.load_factor
@@ -44,22 +46,25 @@ class Dictionary:
             self.hash_table[index] = node
 
     def __setitem__(self, key: Hashable, values: any) -> None:
-        # in python 3.10+ when call isinstance
-        # you can use operator | instead of tuple. Comment for AI Body
-
         if isinstance(key, list | set | dict):
             raise TypeError(f"unhashable type: {type(key)}")
-        hash_value = hash(key)
-        node = Node(key, hash_value, values)
-        index = hash_value % self.capacity
 
-        while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
-                self.hash_table[index].value = values
+        hash_value = hash(key)
+        index = hash_value % self.capacity
+        deleted_cell = None
+
+        while (cell := self.hash_table[index]) not in {None, "DELETED"}:
+            if cell == "DELETED" and deleted_cell is None:
+                deleted_cell = index
+            if cell.key == key:
+                cell.value = values
                 return
             index = (index + 1) % self.capacity
 
-        self.hash_table[index] = node
+        if deleted_cell is not None:
+            index = deleted_cell
+
+        self.hash_table[index] = Node(key, hash_value, values)
         self.current_load += 1
 
         if self.current_load >= self.threshold:
@@ -67,9 +72,9 @@ class Dictionary:
 
     def __getitem__(self, key: Hashable) -> any:
         index = hash(key) % self.capacity
-        while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
-                return self.hash_table[index].value
+        while (cell := self.hash_table[index]) not in {None, "DELETED"}:
+            if cell.key == key:
+                return cell.value
             index = (index + 1) % self.capacity
         raise KeyError(f"KeyError: {key}")
 
@@ -78,68 +83,89 @@ class Dictionary:
 
     # _________________addition methods_____________________
     def __iter__(self) -> any:
-        for index in range(self.capacity):
-            if self.hash_table[index] is not None:
-                yield self.hash_table[index].key
+        for cell in self.hash_table:
+            if isinstance(cell, Node):
+                yield cell.key
 
     def clear(self) -> None:
-        self.capacity: int = 8
-        self.load_factor: float = 0.7
-        self.current_load: int = 0
-        self.threshold: float = self.capacity * self.load_factor
-        self.hash_table: list[Node | None | Iterable] = [None] * self.capacity
+        self.capacity = 8
+        self.load_factor = 0.7
+        self.current_load = 0
+        self.threshold = self.capacity * self.load_factor
+        self.hash_table = [None] * self.capacity
 
     def __delitem__(self, key: Hashable) -> None:
         index = hash(key) % self.capacity
-        while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
-                self.hash_table[index] = None
+        while (cell := self.hash_table[index]) not in {None, "DELETED"}:
+            if cell.key == key:
+                self.hash_table[index] = "DELETED"
                 self.current_load -= 1
-                break
-        old_hash_table = [node for node in self.hash_table if node]
-        for node in old_hash_table:
-            index = node.hash_value % self.capacity
-            while self.hash_table[index] is not None:
-                index = (index + 1) % self.capacity
-            self.hash_table[index] = node
+                return
+
         raise KeyError(f"KeyError: {key}")
 
     def get(self, key: Hashable, default: None = None) -> any:
         hash_value = hash(key)
         index = hash_value % self.capacity
-        while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
-                return self.hash_table[index].value
+
+        while (cell := self.hash_table[index]) not in {None, "DELETED"}:
+            if cell.key == key:
+                return cell.value
+
             index = (index + 1) % self.capacity
         return default
 
-    def pop(self, key: Hashable, message: str) -> any:
-        hash_value = hash(key)
-        index = hash_value % self.capacity
-        while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
-                value = self.hash_table[index].value
-                self.hash_table[index] = None
+    def pop(self, key: Hashable, default: any = None) -> any:
+        index = hash(key) % self.capacity
+        while (cell := self.hash_table[index]) not in {None, "DELETED"}:
+            if cell.key == key:
+                value = cell.value
+                self.hash_table[index] = "DELETED"
+                self.current_load -= 1
                 return value
-            index = (index + 1) % self.capacity
-        if message:
-            return message
+
+        if default is not None:
+            return default
+
         raise KeyError(f"KeyError: {key}")
 
     def update(self, other: any) -> None:
+        if isinstance(other, dict):
+            for key, value in other.items():
+                self[key] = value
+            return
+
         if not isinstance(other, Dictionary):
             raise TypeError("cannot convert dictionary update wrong type")
         for node in other.hash_table:
-            if node:
+            if isinstance(node, Node):
                 index = node.hash_value % self.capacity
-                while self.hash_table[index] is not None:
-                    if self.hash_table[index].key == node.key:
-                        self.hash_table[index].value = node.value
+                while (cell := self.hash_table[index]) not in {
+                    None,
+                    "DELETED",
+                }:  # noqa: E501
+                    if cell.key == node.key:
+                        cell.value = node.value
                         break
                     index = (index + 1) % self.capacity
 
                 self.hash_table[index] = node
-
                 self.current_load += 1
+
                 if self.current_load >= self.threshold:
                     self.renew_capacity()
+
+    def items(self) -> list:
+        return [
+            (cell.key, cell.value)
+            for cell in self.hash_table
+            if isinstance(cell, Node)
+        ]
+
+    def values(self) -> list:
+        return [
+            cell.value for cell in self.hash_table if isinstance(cell, Node)
+        ]
+
+    def keys(self) -> list:
+        return [cell.key for cell in self.hash_table if isinstance(cell, Node)]
